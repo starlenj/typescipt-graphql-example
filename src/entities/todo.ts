1e5e215bba641e24a415787aa66bf326e512560a
starlenj <emreatessoy@gmail.com>
2021-01-02 00:40:29 +0300

    MongoDb Created

---
+++ src/entities/todo.ts
@@ -0,0 +1,5 @@
+export interface todo {
+  id: string;
+  name: string;
+  finished: bool;
+}
--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,11 @@
-console.log("hello world");
+import {MikroORM} from '@mikro-orm/core';
+
+const main = async () => {
+  await MikroORM.init({
+    entities: [],
+    clientUrl: 'mongodb://localhost:27017/todoDb',
+    type: 'mongo',
+  });
+};
+
+main();
